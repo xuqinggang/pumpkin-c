9301b1d1fb1593bad7b99fa4a50e14935506aecc
许庆钢 <xuqinggang@xuqinggangs-MacBook-Air.local>
2017-12-21 18:19:21 +0800

    去掉推荐房源

--- src/components/App/HouseList/RentUnitsSuggest/index.js
+++ src/components/App/HouseList/RentUnitsSuggest/index.js
@@ -12,22 +12,25 @@ export default class RentUnitsSuggest extends Component {
             <div className={clsPrefix}>
                 <div className={`${clsPrefix}--note`}>
                     <i className={`${clsPrefix}--note-icon`} />
-                    <div className={`${clsPrefix}--note-text`}>未找到对应房源，以下是推荐房源</div>
+                    {
+                        // <div className={`${clsPrefix}--note-text`}>未找到对应房源，以下是推荐房源</div>
+                    }
+                    <div className={`${clsPrefix}--note-text`}>未找到对应房源</div>
                 </div>
                 {
-                    this.props.list.map((rentalUnit, index) => (
-                        <RentUnitItem key={index} {...rentalUnit} />
-                    ))
+                    // this.props.list.map((rentalUnit, index) => (
+                    //     <RentUnitItem key={index} {...rentalUnit} />
+                    // ))
                 }
             </div>
         );
     }
 }
 
-RentUnitsSuggest.propTypes = {
-    list: PropTypes.arrayOf(rentUnitShape),
-};
+// RentUnitsSuggest.propTypes = {
+//     list: PropTypes.arrayOf(rentUnitShape),
+// };
 
-RentUnitsSuggest.defaultProps = {
-    list: [],
-};
+// RentUnitsSuggest.defaultProps = {
+//     list: [],
+// };
