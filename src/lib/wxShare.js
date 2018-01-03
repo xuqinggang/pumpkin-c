fa99d0a44ec154537c07e6402d8fe444a339922f
许庆钢 <xuqinggang@xuqinggangs-MacBook-Air.local>
2018-01-03 15:58:43 +0800

    fix

--- src/lib/wxShare.js
+++ src/lib/wxShare.js
@@ -39,6 +39,11 @@ export function wxShare(shareObj = {}) {
                 wx.onMenuShareTimeline(shareObj); //分享到朋友圈
                 wx.onMenuShareAppMessage(shareObj); //分享给朋友
             });
+
+            wx.error(function(res) {
+                alert(JSON.stringify(res));
+                // config信息验证失败会执行error函数，如签名过期导致验证失败，具体错误信息可以打开config的debug模式查看，也可以在返回的res参数中查看，对于SPA可以在这里更新签名。
+            });
         })
 }
 
