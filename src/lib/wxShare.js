04e142ee973ee73e58e4e3ff3deb57983b95bac0
许庆钢 <qinggangxu@sohu-inc.com>
2018-01-07 18:10:06 +0800

    fix

--- src/lib/wxShare.js
+++ src/lib/wxShare.js
@@ -3,6 +3,7 @@ import { isWeiXin, dynamicScript } from 'lib/util';
 
 // 调用焦点，获取用于微信自定义分享的配置信息
 function _ajaxFocusWxConfig(shareObj) {
+    alert(JSON.stringify(shareObj));
     return Service.get('https://wx-open-api.focus.cn/ajax/wxJsConfig', {
         url: encodeURIComponent(shareObj.link),
         sceneType: 6,
@@ -33,12 +34,12 @@ export function wxShare(shareObj = {}) {
             });
 
             wx.ready(function () {
+                alert('!'+JSON.stringify(shareObj));
                 wx.onMenuShareTimeline(shareObj); //分享到朋友圈
                 wx.onMenuShareAppMessage(shareObj); //分享给朋友
             });
 
             wx.error(function(res) {
-                alert(res);
                 // config信息验证失败会执行error函数，如签名过期导致验证失败，具体错误信息可以打开config的debug模式查看，也可以在返回的res参数中查看，对于SPA可以在这里更新签名。
             });
         })
