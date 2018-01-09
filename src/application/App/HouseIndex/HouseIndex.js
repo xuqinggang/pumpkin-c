25bfae3f31f99e6c4ba015e66bfbf45759f9d853
许庆钢 <qinggangxu@sohu-inc.com>
2018-01-09 09:29:11 +0800

    fix

--- src/application/App/HouseIndex/HouseIndex.js
+++ src/application/App/HouseIndex/HouseIndex.js
@@ -1,8 +1,40 @@
 import React, { PureComponent } from 'react';
+import SimpleScroll from 'Shared/SimpleScroll/SimpleScroll';
+import './styles.less';
+
 export default class HouseIndex extends PureComponent {
+    componentDidMount() {
+        console.log('this.wrapDom', this.wrapDom);
+        new SimpleScroll(this.wrapDom);
+    }
     render() {
         return (
-        <div>123</div>
+            <div>
+                <div id="wrapper" ref={(dom) => this.wrapDom = dom}>
+                    <ul id="scroller">
+                        <li>有一美人兮，见之不忘。</li>
+                        <li>一日不见兮，思之如狂。</li>
+                        <li>一日不见兮，思之如狂。</li>
+                        <li>一日不见兮，思之如狂。</li>
+                        <li>凤飞翱翔兮，四海求凰。</li>
+                        <li>无奈佳人兮，不在东墙。</li>
+                        <li>将琴代语兮，聊写衷肠。</li>
+                        <li>何日见许兮，慰我彷徨。</li>
+                        <li>愿言配德兮，携手相将。</li>
+                        <li>凤飞翱翔兮，四海求凰。</li>
+                        <li>无奈佳人兮，不在东墙。</li>
+                        <li>将琴代语兮，聊写衷肠。</li>
+                        <li>何日见许兮，慰我彷徨。</li>
+                        <li>愿言配德兮，携手相将。</li>
+                        <li>凤飞翱翔兮，四海求凰。</li>
+                        <li>无奈佳人兮，不在东墙。</li>
+                        <li>将琴代语兮，聊写衷肠。</li>
+                        <li>何日见许兮，慰我彷徨。</li>
+                        <li>愿言配德兮，携手相将。</li>
+                        <li>不得於飞兮，使我沦亡。</li>
+                    </ul>
+                </div>
+            </div>
         )
     }
 }
--- src/components/Shared/SimpleScroll/SimpleScroll.js
+++ src/components/Shared/SimpleScroll/SimpleScroll.js
@@ -1,17 +1,21 @@
-class SimpleScroll {
+const rAF = window.requestAnimationFrame || window.webkitRequestAnimationFrame || function(callback) {
+    setTimeout(callback, 17);
+};
+
+export default class SimpleScroll {
     constructor(wrapperDom, options) {
 
         // 包裹容器dom和滚动元素dom
         this.wrapperDom = wrapperDom;
         this.scrollerDom = wrapperDom.children[0];
 
-        this.wrapperDomInfo = this.wrapperDoom.getBoundingClientRect();
+        this.wrapperDomInfo = this.wrapperDom.getBoundingClientRect();
         const { width: wrapperWidth, height: wrapperHeight } = this.wrapperDomInfo;
         // 包裹容器的宽高
         this.wrapperWidth = wrapperWidth;
         this.wrapperHeight = wrapperHeight;
 
-        this.scrollerDomInfo = this.scrollerDomInfo.getBoundingClientRect();
+        this.scrollerDomInfo = this.scrollerDom.getBoundingClientRect();
         const { width: scrollerWidth, height: scrollerHeight } = this.scrollerDomInfo;
         // 滚动元素的宽高
         this.scrollerWidth = scrollerWidth;
@@ -45,6 +49,8 @@ class SimpleScroll {
         this.startTime = null;
         // 触摸过程中最新的时间
         this.lastTime = null;
+        // touchend 时间
+        this.endTime = null;
 
         // 触摸位置信息
         this.position = {
@@ -54,24 +60,141 @@ class SimpleScroll {
             // 触摸的最新的位置
             lastX: 0,
             lastY: 0,
-            // translate的终点
+            // translate的实时的位置
             translateX: 0,
             translateY: 0,
+            // 触摸结束后，translate的结束位置
+            translateEndX: 0,
+            translateEndY: 0,
         };
-
+        this._initStyle();
         this._initEvent();
     }
-    _init() {
-
+    _initStyle() {
+        this.wrapperDom.style.overflow = "hidden";
+        this.scrollerDom.style.minHeight = "100%";
     }
     _initEvent() {
-        document.addEventListener('touchstart', this._touchstart, false);
+        document.addEventListener('touchstart', this._touchStart, false);
     }
     _refresh(isExecRefreshEvent) {
 
     }
-    _scrollTo(desX, desY) {
-      this.scrollerDom.style.transform = `translate3d(${desX}, ${desY}, 0)`;
+    // 计算x,y的值
+    _compute(val, min, max) {
+        // console.log('val', val, min, max);
+        const {
+            isBounce,
+        } = this.options;
+        if (val > min) {
+            if (isBounce && (val > (min + 10))) {
+                return Math.round(min + ((val - min) / 4));
+            } else {
+                return min;
+            }
+        }
+
+        if (val < max) {
+            if (isBounce && (val < (max - 10))) {
+                return Math.round(max + ((val - max) / 4));
+            } else {
+                return max;
+            }
+        }
+
+        return val;
+    }
+    
+    /**
+     * 在指定时间内将指定元素从开始位置移到结束位置并执行回调方法
+     * el 必须是dom元素，必填
+     * x,y 结束位置，必填
+     * duration 过渡时长，单位ms，可选
+     * callback 回调方法，可选
+     */
+    _moveTo(el, translateX, translateY, duration, callback) {
+        console.log('translateY _moveTo', translateY)
+        let startX = 0,
+            startY = 0,
+            endX, endY, zoom = 1,
+            stepX, stepY, result;
+        result = /translate3d\(([\-\d\.]+)px,\s+([\-\d\.]+)px, \s+([\-\d\.]+px)\)/g.exec(el.style.transform);
+        if (result) {
+            startX = Number(result[1]);
+            startY = Number(result[2]);
+            // zoom = Number(result[3]);
+        }
+        duration = duration || 17;
+        stepX = (translateX - startX) / (duration / 17);
+        stepY = (translateY - startY) / (duration / 17);
+        endX = startX;
+        endY = startY;
+        console.log('stepY', stepY)
+        function moving() {
+            duration = duration - 17;
+            console.log('duration', duration);
+            if (duration <= 0) {
+                endX = translateX;
+                endY = translateY;
+            } else {
+                endX = parseInt(endX + stepX, 10);
+                endY = parseInt(endY + stepY, 10);
+            }
+            console.log('endY', endY, translateY)
+            el.style.transform = `translate3d(${endX}px, ${endY}px, 0px)`;
+
+            if (duration > 0 && !(endX === translateX && endY === translateY)) {
+                rAF(moving);
+            } else if (typeof callback === "function") {
+                callback();
+            }
+        }
+
+        moving();
+    }
+    /**
+     * allow: 是否允许超出边界。false:释放回弹
+     * system: false: 手动调用或者move结束时调用
+     */
+    scrollTo(translateX, translateY, timing, allow, callback, system) {
+        let tmpY = translateY,
+            tmpX = translateX;
+        if (!allow) {
+            tmpY = translateY >= this.minScrollY ? this.minScrollY : 
+                (translateY <= this.maxScrollY) ? this.maxScrollY : translateY;
+            console.log('tmpY', tmpY)
+        }
+        this.position.translateY = tmpY;
+
+        if (!system) {
+            this.position.translateEndX = tmpX;
+            this.position.translateEndY = tmpY;
+        }
+
+        if (timing) {
+            console.log('timing', this.position.translateY)
+            this._moveTo(this.scrollerDom, this.position.translateX, this.position.translateY, timing, callback);
+        } else {
+            this._scrollTo(this.position.translateX, this.position.translateY);
+        }
+    }
+    _scrollTo(translateX, translateY) {
+      this.scrollerDom.style.transform = `translate3d(${translateX}px, ${translateY}px, 0px)`;
+    }
+    // touchmove 实时滚动
+    _doScroll() {
+        const {
+            minScrollX,
+            maxScrollX,
+            minScrollY,
+            maxScrollY,
+        } = this;
+        if (this.options.isBounce) {
+            // 设置回弹，重新计算滚动元素的实时位置
+            this.position.translateY = this._compute(this.position.translateY, minScrollY, maxScrollY);
+            // console.log('_compute', this.position.translateY);
+            this.scrollTo(this.position.translateX, this.position.translateY, 0, true, null, true)
+        }
     }
     _start(e) {
         const touches = e.touches || [e];
@@ -88,7 +211,7 @@ class SimpleScroll {
     }
     _move(e) {
         const touches = e.touches || [e];
-        const x, y, deltaX, deltaY;
+        let x, y, deltaX, deltaY;
 
         x = touches[0].pageX;
         y = touches[0].pageY;
@@ -103,27 +226,43 @@ class SimpleScroll {
         this.position.lastX = x;
         this.position.lastY = y;
 
-        if (m.status === 'PRESCROLL') {
+        if (this.status === 'PRESCROLL') {
             const { isScrollY, isScrollX } = this.options;
-        //判断为y方向，y方向滑动较常使用，因此优先判断
+            //判断为y方向，y方向滑动较常使用，因此优先判断
 
             if (isScrollY && (!isScrollX || Math.abs(deltaY) > Math.abs(deltaX))) {
-                m.status = 'SCROLLINGY'
-
+                this.status = 'SCROLLINGY';
             }
 
             return;
         }
 
-        if (m.status === 'SCROLLINGY') {
+        if (this.status === 'SCROLLINGY') {
+            console.log('y-startY', y, this.position.startY, y - this.position.startY);
+            this.position.translateY = y - this.position.startY + this.position.translateEndY;
+            // console.log('SCROLLINGY', this.position.translateY, this.position.translateEndY);
+            this._doScroll();
+        }
+    }
+    _end(e) {
+        const {
+            translateX,
+            translateY,
+        } = this.position;
 
+        let isBeyoundY, isBeyoundX;
+        this.endTime = Date.now();
+
+        if (this.status === 'SCROLLINGY' || this.status === 'SCROLLINGX') {
+            isBeyoundY = translateY > this.minScrollY || translateY < this.maxScrollY;
+            isBeyoundX = translateX > this.minScrollX || translateX < this.maxScrollX;
+
+            console.log('translateY', translateY)
+                this.scrollTo(translateX, translateY, 300, false);
         }
 
     }
-    _end(e) {
-
-    }
-    _touchStart(e) {
+    _touchStart = (e) => {
         if (!this.options.isScroll) {
             return;
         }
@@ -134,13 +273,14 @@ class SimpleScroll {
         }
 
         this._start(e);
+        // console.log('this.position', this.position.translateEndY, this.position.translateY);
         document.addEventListener('touchmove', this._touchMove, false);
         document.addEventListener('touchend', this._touchEnd, false);
     }
-    _touchMove(e) {
+    _touchMove = (e) => {
         this._move(e);
     }
-    _touchEnd(e) {
-
+    _touchEnd = (e) => {
+        this._end(e);
     }
 }
