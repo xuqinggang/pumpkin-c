8893e06563ce1d625fa0773f449ffa7058cc9d26
孔令星(产品技术) <lingxingkong213712@sohu-inc.com>
2018-02-24 17:24:13 +0800

    Apartment

--- src/components/App/ApartmentList/ApartmentItem/index.js
+++ src/components/App/ApartmentList/ApartmentItem/index.js
@@ -8,24 +8,33 @@ const classPrefix = 'm-apartmentitem';
 const Mdata = {
     image: 'https://pic.kuaizhan.com/g3/d2/21/e009-03b9-46ca-861f-b1ce3e9a8f9a12?crop=1&cpos=middle&w=255&h=255',
     floorPlanCount: 5,
-    RentCount: 20,
+    rentCount: 20,
     title: '乐乎公寓立水桥店',
     price: 3500,
     location: '昌平区立水桥东小口镇中滩村105号',
 };
 
 export default class ApartmentItem extends PureComponent {
-    constructor(props) {
-        super(props);
-        this.state = {};
-    }
-
-    componentWillMount() {}
-
     render() {
         return (
             <div className={`${classPrefix}`}>
-                <div>{Mdata.title}</div>
+                <div className={`${classPrefix}-image`}>
+                    <img src={Mdata.image} alt="优质公寓" />
+                    <div className={'tip'}>
+                        <span>户型/{Mdata.floorPlanCount}个</span>
+                        <span>可租/{Mdata.rentCount}套</span>
+                    </div>
+                </div>
+                <div className={`g-grid-row f-flex-justify-between ${classPrefix}-title-price`}>
+                    <span className={`${classPrefix}-title`}>{Mdata.title}</span>
+                    <span className={`${classPrefix}-price`}>{Mdata.price}
+                        <span className={`${classPrefix}-unit`}>元/月</span>
+                    </span>
+                </div>
+                <div>
+                    <span className={'icon-region'} />
+                    <span>{Mdata.location}</span>
+                </div>
             </div>
         );
     }
--- src/components/App/ApartmentList/PureApartmentList/index.js
+++ src/components/App/ApartmentList/PureApartmentList/index.js
@@ -1,28 +1,65 @@
 import React, { PureComponent } from 'react';
+import PropTypes from 'prop-types';
 import ApartmentItem from '../ApartmentItem';
+import { getDocHeight, getScrollTop } from 'lib/util';
 
 import './styles.less';
+
+const classPrefix = 'm-pureapartmentlist';
 
 export default class PureApartmentList extends PureComponent {
     constructor(props) {
         super(props);
-        this.state = {};
+        this.lastScrollTop = 0;
     }
-
-    componentWillMount() {}
-
+    componentDidMount() {
+        window.addEventListener('scroll', this.handleLoadMore);
+    }
+    componentWillUnmount() {
+        window.removeEventListener('scroll', this.handleLoadMore);
+    }
+    handleLoadMore = () => {
+        const reserveSize = 200;
+        const scrollTop = getScrollTop();
+        // console.log(scrollTop, getDocHeight(), window.innerHeight);
+        if (scrollTop > this.lastScrollTop) {
+            // 向下滚动
+            if ((getDocHeight() - window.innerHeight - scrollTop) <= reserveSize) {
+                const { curPage, totalPage } = this.props.pager;
+                if (curPage < totalPage) {
+                    console.log('ssss-0---');
+                    // this.props.onLoadMore();
+                }
+            }
+        }
+        this.lastScrollTop = scrollTop;
+    }
     render() {
         return (
-            <div>
+            <div className={`${classPrefix}`}>
                 {
-                    new Array(89).fill(1).map((x, i) => (
-                        <div>
-                            <ApartmentItem />
-                            <p>{i + 1}</p>
-                        </div>
+                    new Array(89).fill(1).map(() => (
+                        <ApartmentItem />
                     ))
                 }
             </div>
         );
     }
 }
+
+PureApartmentList.propTypes = {
+    list: PropTypes.arrayOf({}),
+    onLoadMore: PropTypes.func,
+    loading: PropTypes.bool,
+    pager: PropTypes.object,
+};
+
+PureApartmentList.defaultProps = {
+    list: [],
+    onLoadMore: () => {},
+    loading: false,
+    pager: {
+        curPage: 1,
+        totalPage: 1,
+    },
+};
