407cfc5f6a44a099187ecfee693b6ee1e754ecdd
许庆钢 <qinggangxu@sohu-inc.com>
2018-03-23 10:50:31 +0800

    improve:add flow&&eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,22 +1,5 @@
 module.exports = {
-    root: true,
-    parserOptions: {
-        ecmascript: 6,
-        sourceType: 'module',
-    },
-    env: {
-        browser: true,
-        node: true,
-        mocha: true,
-    },
-    extends: 'kuaizhan',
-    rules: {
-        'react/prefer-stateless-function': 'off',
-        'react/jsx-indent-props': [1, 4],
-        'react/no-array-index-key': 'off',
-        'react/react-in-jsx-scope': 'off',
-        'jsx-a11y/label-has-for': 'off',
-    },
+    extends: 'xuqinggang',
     settings: {
         'import/resolver': {
             webpack: {
