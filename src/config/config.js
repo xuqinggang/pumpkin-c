6c9de6a0091e89237bf1eb2ac2e0b525039fd8df
许庆钢 <qinggangxu@sohu-inc.com>
2018-03-27 16:54:57 +0800

    add feature:添加切换城市

--- src/config/config.js
+++ src/config/config.js
@@ -1,6 +1,16 @@
 export default {
-    prefixMapCityId: {
-        'bj': 1,
-        'sh': 73,
+    AbbrevMapCity: {
+        bj: {
+            text: '北京',
+            id: 1,
+        },
+        sh: {
+            text: '上海',
+            id: 73,
+        }
+    },
+    CityeTextMapAbbrev: {
+        '北京': 'bj',
+        '上海': 'sh',
     },
 }
