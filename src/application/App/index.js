3c3f333ed1d4313ccc0158a9e9405dcdc174e468
许庆钢 <qinggangxu@sohu-inc.com>
2018-03-28 16:19:27 +0800

    fix bug:eslint

--- src/application/App/index.js
+++ src/application/App/index.js
@@ -1,6 +1,5 @@
-import React from 'react';
 import { hydrate } from 'react-dom';
-import injectTapEventPlugin from "react-tap-event-plugin";
+import injectTapEventPlugin from 'react-tap-event-plugin';
 import createHistory from 'history/createBrowserHistory';
 
 // 全局样式
