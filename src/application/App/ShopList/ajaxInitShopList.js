7e31ed5ec9c13f53716ab0eb3f019edad484da68
孔令星(产品技术) <lingxingkong213712@sohu-inc.com>
2018-04-20 17:14:52 +0800

    fix

--- src/application/App/ShopList/ajaxInitShopList.js
+++ src/application/App/ShopList/ajaxInitShopList.js
@@ -7,6 +7,7 @@ export function ajaxGetBrandList() {
         .then((res) => {
             if (res.code === 200) {
                 return res.data;
+            }
 
             throw new Error(res);
         });
