70dc780c1cf6aee284c3a22fef93446d4a2a22f0
孔令星(产品技术) <lingxingkong213712@sohu-inc.com>
2018-05-11 14:58:15 +0800

    fix

--- src/application/App/Apartment/ApartmentIndex/index.js
+++ src/application/App/Apartment/ApartmentIndex/index.js
@@ -75,7 +75,7 @@ export default class ApartmentIndex extends PureComponent {
             title: `${apartment.name}`,
             link: window.location.href.split('#')[0],
             imgUrl,
-            desc: '住品牌公寓，享品质生活!',
+            desc: '住品牌公寓，上南瓜租房!',
         };
         callback(data);
     }
